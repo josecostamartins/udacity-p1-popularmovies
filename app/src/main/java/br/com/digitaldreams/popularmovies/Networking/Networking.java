cd5d5f605a6ef1d49dec86ef6ff2df22d82002e8
josecostamartins <josecostamartins@gmail.com>
2016-07-25 18:50:09 -0300

    working gitignore 2

---
+++ app/src/main/java/br/com/digitaldreams/popularmovies/Networking/Networking.java
@@ -0,0 +1,29 @@
+package br.com.digitaldreams.popularmovies.Networking;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by josecostamartins on 7/25/16.
+ */
+public class Networking extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
