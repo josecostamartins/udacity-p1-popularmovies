b6ce1a33aebf5bb7ef5d1e7eb923d4f0618111c6
josecostamartins <josecostamartins@gmail.com>
2016-07-25 18:49:25 -0300

    working gitignore

--- app/src/main/java/br/com/digitaldreams/popularmovies/Utils/Definitions.java
+++
@@ -1,8 +0,0 @@
-package br.com.digitaldreams.popularmovies.Utils;
-
-/**
- * Created by josecostamartins on 7/24/16.
- */
-public abstract class Definitions {
-
-}
--- app/src/main/java/br/com/digitaldreams/popularmovies/adapter/MovieRecyclerAdapter.java
+++
@@ -1,7 +0,0 @@
-package br.com.digitaldreams.popularmovies.adapter;
-
-/**
- * Created by josecostamartins on 7/24/16.
- */
-public class MovieRecyclerAdapter {
-}
